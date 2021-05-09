c1d269bd15ac3e49de7f5eb4084f21906ba7ff4d
Harmanjit Singh <harmannamrah@gmail.com>
2021-05-10 00:19:27 +0530

    complete the content section display, add animations, make a expand/contract button

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,14 @@ $(document).ready(function(){
 
     const jumbotron = $('#jumbotron');
     const pageHeading = $('#page-heading');
-    const contentSection = $('section');
-
+    const contentSection = $('#content');
+    const form = $('form');
+    const expandContractButton = $('#expand-button');
+    const tasksContainer = $('#tasks-container');
 
 
     // --------------------------------------------------------------
-    // ANIMATING THE HEADING
+    // ANIMATING THE HEADING & CONTENT SECTION
 
     setTimeout(animatePageHeading,1000);
 
@@ -19,7 +21,7 @@ $(document).ready(function(){
         setTimeout(function(){
 
             //this rolls up the contentSection
-            contentSection.css('display','block');
+            contentSection.css('display','flex');
             contentSection.addClass('animate-section');
 
             //shifting and scaling down the page heading font size
@@ -31,8 +33,44 @@ $(document).ready(function(){
             })
         },2500);
     }
+    // ----------------------------------------------------------------
+    // ANIMATING THE FORM EXPAND AND CONTRACT
+    let expanded=true; 
 
+    expandContractButton.on('click',function(){
+        if(!expanded){
+            //means form is closed and tasksContainer is in bigger view
+           
+            form.removeClass('contract');
+            tasksContainer.removeClass('tasks-container-expand');
 
+            form.addClass('expand');
+            tasksContainer.addClass('tasks-container-contract');
+            
+            setTimeout(function(){
+                form.css('height','45%');
+                tasksContainer.css('height','45%');
+            },580);
+            
+            expanded = true;
+
+        }else{
+            //this is the deafult view of FORM which is EXPANDED
+            form.removeClass('expand');
+            tasksContainer.removeClass('tasks-container-contract');
+
+            form.addClass('contract');
+            tasksContainer.addClass('tasks-container-expand');
+            setTimeout(function(){
+                form.css('height','0%');
+                tasksContainer.css('height','95%');
+               
+            },580);
+
+            expanded = false; 
+        }
+        
+    });
 
 
 
